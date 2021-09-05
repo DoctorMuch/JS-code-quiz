7af8ebf6d4dcf45a1e8bda447a97a749eff04e8c
Doctor Mud <davidmueth@gmail.com>
2021-09-05 17:45:28 -0500

    basic scoring and time penalty built

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,15 +1,25 @@
 let timerEl = document.getElementById("timer");
 let startBtnEl = document.getElementById("start");
-let questionEl = document.getElementById("question");
-let answerEl = document.getElementById("answer");
+let introEl = document.getElementById("intro");
+let introP = document.getElementById("introP");
+let questionEl = document.getElementById("quiz-start");
+let timeLeft = 75;
 
-let questions = {
+let questions = [
+  {
     question: "Which of the following is not a common data type in JavaScript?",
-    answer: ["boolean", "string", "number", "text"]
-};
+    answer: ["boolean", "string", "number", "text"],
+    correct: "text"
+  },
+
+  {
+    question: "What is required to call a function?",
+    answer: ["a button", "parentheses", "a parameter", "an argument"],
+    correct: "parentheses"
+  }
+]
 
 function countDown(){
-  let timeLeft = 75;
 
   let timerInterval = setInterval(function(){
     if (timeLeft > 0) {
@@ -27,28 +37,58 @@ function countDown(){
 
 let startQuiz = function(){
   countDown();
-  let clearStart = document.getElementById("quiz-start");
-  clearStart.removeChild(startBtnEl);
+  clearStart();
   // Ask first question
-  questionEl.textContent = questions.question;
+  askQuestion();
   // Display possible answers
-  makeList();
+  displayAnswers();
 };
 
-// Turn answers array into list
-let makeList = function(){
-  answerEl.textContent= '';
-  let answerOptions = document.createElement("ol");
-  answer.appendChild(answerOptions)
-  for (let i=0; i < 4; i++){
-    let answerOption = document.createElement("li");
-    answerOption.textContent = questions.answer[i];
-    answerOptions.appendChild(answerOption);
-  }
+let clearStart = function(){
+  questionEl.removeChild(startBtnEl);
+  questionEl.removeChild(introEl);
+  questionEl.removeChild(introP);
 }
 
-let endQuiz = function(){
+let askQuestion = function(){
+  questionEl.className = "quizText";
+  let askQuestionEl = document.createElement("h2");
+  askQuestionEl.className = "questionText";
+  askQuestionEl.textContent = questions[0].question;
+  questionEl.appendChild(askQuestionEl);
+};
 
+let displayAnswers = function(){
+  let pointTotal = 0;
+  let answerOptionsEl = document.createElement("ol");
+  answerOptionsEl.className = "answers";
+  questionEl.appendChild(answerOptionsEl);
+  let quizFooter = document.createElement("div");
+  quizFooter.className = "statusBar";
+  for (let i=0;i<4;i++){
+    let answerOptionEl = document.createElement("input");
+    answerOptionEl.type = "button";
+    answerOptionEl.id = "submit";
+    answerOptionEl.value = questions[0].answer[i];
+    answerOptionEl.className = "inputBtn";
+    answerOptionEl.addEventListener("click", function(){
+      if (answerOptionEl.value === questions[0].correct){
+        pointTotal +=5;
+        quizFooter.textContent = "Correct!";
+        questionEl.appendChild(quizFooter);
+      }
+      else {
+        timeLeft -= 10;
+        quizFooter.textContent = "Wrong!";
+        questionEl.appendChild(quizFooter);
+      }
+    })
+    answerOptionsEl.appendChild(answerOptionEl);
+  } 
+};
+
+let endQuiz = function(){
+  alert("Time is up! Let's see how you did!");
 };
 
 startBtnEl.onclick = startQuiz;
